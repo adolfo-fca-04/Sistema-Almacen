f0ae2af59e0453e5ad4ebf4264ae5ccc369fa522
adolfo-fca-04 <adolfo.fca.04@outlook.com>
2026-08-01 04:26:44 -0500

    Primer comit

---
+++ src/test/java/com/alamacen/sistema_almacen/SistemaAlmacenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alamacen.sistema_almacen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAlmacenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
