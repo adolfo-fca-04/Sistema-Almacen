ab00c42178fb885cc93509d74ca921d9f5ca7cc7
adolfo-fca-04 <adolfo.fca.04@outlook.com>
2026-08-12 19:53:14 -0500

    Modulo Productos

--- src/main/java/com/almacen/sistema_almacen/controller/CategoriaController.java
+++ src/main/java/com/almacen/sistema_almacen/controller/CategoriaController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/categorias")
@@ -24,14 +25,26 @@ public class CategoriaController {
         model.addAttribute("categorias", categoriaService.listarTodas());
         // Enviamos un objeto Categoria vacío para vincularlo al formulario HTML
         model.addAttribute("nuevaCategoria", new Categoria());
-        return "categorias/index"; // "templates/categorias/index.html"
+        return "categorias/index";
     }   
     
-    //Procesar la información enviada desde el formulario HTML (POST)
     @PostMapping("/guardar")
     public String guardarCategoria(@ModelAttribute("nuevaCategoria") Categoria categoria) {
         categoriaService.guardar(categoria);
-        // Redirige nuevamente a la lista de categorías para ver el cambio reflejado
+        return "redirect:/categorias";
+    }
+    
+    @GetMapping("/editar/{id}")
+    public String mostrarFormularioEditar(@PathVariable("id") Long id, Model model) {
+        Categoria categoriaExistente = categoriaService.obtenerPorId(id);
+        model.addAttribute("categorias", categoriaService.listarTodas());
+        model.addAttribute("nuevaCategoria", categoriaExistente); // Pasamos el objeto lleno
+        return "categorias/index";
+    }
+
+    @GetMapping("/eliminar/{id}")
+    public String eliminarCategoria(@PathVariable("id") Long id) {
+        categoriaService.eliminar(id);
         return "redirect:/categorias";
     }
 }
